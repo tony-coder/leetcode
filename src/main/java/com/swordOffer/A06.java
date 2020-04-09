94e731bbd1cc1326bd5179d49c237602567437d6
tony-coder <1606771552@qq.com>
2020-04-09 15:51:35 +0800

    add 面试题06. 从尾到头打印链表

---
+++ src/main/java/com/swordOffer/A06.java
@@ -0,0 +1,43 @@
+package com.swordOffer;
+
+import java.util.Stack;
+
+/**
+ * 面试题06. 从尾到头打印链表
+ * https://leetcode-cn.com/problems/cong-wei-dao-tou-da-yin-lian-biao-lcof/
+ * 直接遍历、用栈、递归都可以实现
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
+
+public class A06 {
+    public static int[] reversePrint(ListNode head) {
+        Stack<Integer> stack = new Stack<>();
+        ListNode cur = head;
+        while (cur != null) {
+            stack.push(cur.val);
+            cur = cur.next;
+        }
+        int[] ans = new int[stack.size()];
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = stack.pop();
+        }
+        return ans;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
